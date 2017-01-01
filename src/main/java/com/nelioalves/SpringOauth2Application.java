30e8e1fae6ef38f61def9560a2cc499ffbaa8d73
Nelio Alves <nelio.iftm@gmail.com>
2017-01-01 14:11:47 -0200

    Added click feature

--- src/main/java/com/nelioalves/SpringOauth2Application.java
+++ src/main/java/com/nelioalves/SpringOauth2Application.java
@@ -2,10 +2,8 @@ package com.nelioalves;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 public class SpringOauth2Application {
 
 	public static void main(String[] args) {
