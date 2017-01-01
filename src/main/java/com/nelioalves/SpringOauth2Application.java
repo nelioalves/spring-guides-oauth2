ac2b847da3a5b5301361e04aef9c91ad820d9ea6
Nelio Alves <nelio.iftm@gmail.com>
2017-01-01 13:32:46 -0200

    Finished simple feature

--- src/main/java/com/nelioalves/SpringOauth2Application.java
+++ src/main/java/com/nelioalves/SpringOauth2Application.java
@@ -2,8 +2,10 @@ package com.nelioalves;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringOauth2Application {
 
 	public static void main(String[] args) {
